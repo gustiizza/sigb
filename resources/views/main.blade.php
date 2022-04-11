ca86c8534ba8f5518a04c58e3f7a506a41645133
gustiizza <gustiizza.64@gmail.com>
2022-04-11 15:11:09 +0700

    installation laravel

---
+++ resources/views/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SIG</title>
+</head>
+<body>
+<h2>Universitas Tanjungpura</h2>
+
+<p></p>
+<img src="{{ url('/img/untan.png') }}" alt="Untan">
+<map name="workmap">
+    <area shape="circle" coords="337,300,44" href="coffee.htm" onclick="myFunction()">
+  </map>
+
+</body>
+</html>
