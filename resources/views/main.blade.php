1d117667f569e62b17d72cefd6d75c06594c3f4f
gustiizza <gustiizza.64@gmail.com>
2022-04-13 21:57:02 +0700

    menambahkan map dan point map untan

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,15 +5,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SIG</title>
-</head>
-<body>
-<h2>Universitas Tanjungpura</h2>
 
-<p>Tata Letak Lokasi Universitas Tanjungpura</p>
-<img src="{{ url('/img/untan.png') }}" alt="Untan">
-<map name="workmap">
-    <area shape="circle" coords="337,300,44" href="coffee.htm" onclick="myFunction()">
-  </map>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+    crossorigin=""/>
+
+    <style>
+        #map { height: 680px; }
+        header{
+            display:block;
+        }
+        h1{
+            text-align: center;
+        }
+    </style>
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="crossorigin="">
+    </script>
+
+</head>
+
+<body>
+    <header>
+        <h1> Sistem Informasi Geografis <br>
+            Universitas Tanjungpura</h1>
+      </header>
+    <div id="map">
+    </div>
+
 
 </body>
+<script>
+    var map = L.map('map').setView([-0.0602507,109.3454158], 20);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+    L.marker([-0.0602507,109.3454158]).addTo(map)
+    .bindPopup('Universitas Tanjungpura')
+    .openPopup();
+    var popup = L.popup();
+    function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+    }
+    map.on('click', onMapClick);
+</script>
 </html>
